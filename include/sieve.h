5e031c4f57d796ca0f20ee35e2e5d432ba2bd3c8
Glenn <hazelnutvt04@gmail.com>
2020-04-03 00:12:46 -0400

    Initial commit

---
+++ include/sieve.h
@@ -0,0 +1,8 @@
+#ifndef __SIEVE_H__
+#define __SIEVE_H__
+
+#include "utils.h"
+
+void sieve( int* __restrict__ primes, int* num_primes, int max );
+
+#endif // end of #ifndef __SIEVE_H__
--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,17 @@
 // C++ File for main
 
-#include <cstdio>
-#include <stdlib.h>
-#include <cmath>
-
-#include <gsl/gsl_math.h>
-#include <gsl/gsl_complex_math.h>
-
-#include "utils.h"
+#include "sieve.h"
 
 int main( int argc, char* argv[] ) {
-  
-  int num_vals = 256;
-  float vals[num_vals];
+   int val = 50;
+   int num_primes = 0;
+   int* primes = ( int* )calloc( val, sizeof(int) );
+   
+   printf( "Val is %d\n", val );
+   sieve( primes, &num_primes, val );
 
-  float upper = 2000.0;
-  float lower = 1500.0;
-
-  gen_vals<float>( vals, upper, lower, num_vals );  
-  printf("Input Values:\n");
-  printf_floats( vals, 10 );
-
-  int order = 2;
-  float m[order];
-  gsl_complex p[order];
-  //m = numpy.arange(-N+1, N, 2)
-  m[0] = -1;
-  m[1] = 1;
-
-  //p = -numpy.exp(1j * pi * m / (2 * N))
-  double temp_x = M_PI/(2*order);
-  printf("temp_x is %13.12f\n", temp_x );
-  for( int index = 0; index < order; index++ ) {
-     double temp_mx = m[index] * temp_x;
-     printf("temp_mx is %13.12f\n", temp_mx );
-
-     gsl_complex ctemp_mx = gsl_complex_rect( 0.0, temp_mx );
-     p[index] = gsl_complex_negative(gsl_complex_exp( ctemp_mx ));
-     printf("p[%d] = %f + j%f\n", index, GSL_REAL(p[index]), GSL_IMAG(p[index]) );
-  }
-  return 0;
-}
-
-// end of C++ file for main
+   for ( int index = 0; index < num_primes; index++ ) {
+      printf( "%d) %d\n", index, primes[index] );
+   }
+   printf( "\n" );
+} 
