421d753c3191e571924686892aaaaa30030c4cd3
Glenn <hazelnutvt04@gmail.com>
2020-04-14 02:17:08 -0400

    Updated to test prime_decomposition

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,36 @@
 // C++ File for main
 
-#include <cstdio>
-#include <stdlib.h>
-#include <cmath>
+#include "prime_decomposition.h"
 
-#include <gsl/gsl_math.h>
-#include <gsl/gsl_complex_math.h>
+void usage( const char* prog_name ) {
+   printf( "Usage: %s <integer>\n", prog_name );
+   printf( "\tReturns list of prime numbers which\n" );
+   printf( "\twhen all multiplied together, are\n" );
+   printf( "\tequal to that number.\n" ); 
+}
 
-#include "utils.h"
+int main( int argc, char** argv ) {
+   int val = 2;
+   if ( argc > 1 ) {
+      val = strtol( argv[1], NULL, 10 );
+   } else {
+      usage( argv[0] );
+      exit( EXIT_FAILURE );
+   }
 
-int main( int argc, char* argv[] ) {
-  
-  int num_vals = 256;
-  float vals[num_vals];
+   int prime_factors[val];
+   int num_prime_factors = 0;
+   bool debug = true;
 
-  float upper = 2000.0;
-  float lower = 1500.0;
+   prime_decomposition( &(prime_factors[0]), &num_prime_factors, val, debug );
 
-  gen_vals<float>( vals, upper, lower, num_vals );  
-  printf("Input Values:\n");
-  printf_floats( vals, 10 );
+   printf( "Prime Factors of %d: { ", val ); 
+   for( int index = 0; index < num_prime_factors; index++ ) {
+      printf( "%d%s ", prime_factors[index], ( ( index < (num_prime_factors-1) ) ? ",":"" ) ); 
+   }
+   printf( "}\n" ); 
 
-  int order = 2;
-  float m[order];
-  gsl_complex p[order];
-  //m = numpy.arange(-N+1, N, 2)
-  m[0] = -1;
-  m[1] = 1;
-
-  //p = -numpy.exp(1j * pi * m / (2 * N))
-  double temp_x = M_PI/(2*order);
-  printf("temp_x is %13.12f\n", temp_x );
-  for( int index = 0; index < order; index++ ) {
-     double temp_mx = m[index] * temp_x;
-     printf("temp_mx is %13.12f\n", temp_mx );
-
-     gsl_complex ctemp_mx = gsl_complex_rect( 0.0, temp_mx );
-     p[index] = gsl_complex_negative(gsl_complex_exp( ctemp_mx ));
-     printf("p[%d] = %f + j%f\n", index, GSL_REAL(p[index]), GSL_IMAG(p[index]) );
-  }
-  return 0;
+   exit( EXIT_SUCCESS );
 }
 
 // end of C++ file for main
